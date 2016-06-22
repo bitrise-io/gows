7a5cd4dbdc46739ede3246c12e91523787c68680
Viktor Benei <viktor.benei@gmail.com>
2016-06-22 17:30:44 +0200

    gows workspaces : now highlights the current project, if found

--- cli/list_workspaces.go
+++ cli/list_workspaces.go
@@ -2,7 +2,10 @@ package cli
 
 import (
 	"fmt"
+	"os"
 
+	log "github.com/Sirupsen/logrus"
+	"github.com/bitrise-io/go-utils/colorstring"
 	"github.com/bitrise-tools/gows/config"
 	"github.com/urfave/cli"
 )
@@ -13,10 +16,19 @@ func listWorkspacesCmd(c *cli.Context) error {
 		return fmt.Errorf("Failed to load gows config: %s", err)
 	}
 
+	currWorkDir, err := os.Getwd()
+	if err != nil {
+		log.Debugf("Failed to get current working directory: %s", err)
+	}
+
 	fmt.Println()
 	fmt.Println("=== Registered gows [project -> workspace] path list ===")
 	for projectPath, wfConfig := range gowsConfig.Workspaces {
-		fmt.Printf(" * %s -> %s\n", projectPath, wfConfig.WorkspaceRootPath)
+		if projectPath == currWorkDir {
+			fmt.Println(colorstring.Greenf(" * %s -> %s", projectPath, wfConfig.WorkspaceRootPath))
+		} else {
+			fmt.Printf(" * %s -> %s\n", projectPath, wfConfig.WorkspaceRootPath)
+		}
 	}
 	fmt.Println("========================================================")
 	fmt.Println()
