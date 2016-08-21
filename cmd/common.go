95bafd079eee9a70792644a64a61bab4eb0ff945
Viktor Benei <viktor.benei@gmail.com>
2016-08-21 09:08:09 +0200

    base functions moved to gows/gows

--- cmd/common.go
+++ cmd/common.go
@@ -16,6 +16,7 @@ import (
 	"github.com/bitrise-io/go-utils/fileutil"
 	"github.com/bitrise-io/go-utils/pathutil"
 	"github.com/bitrise-tools/gows/config"
+	"github.com/bitrise-tools/gows/gows"
 )
 
 const (
@@ -76,7 +77,7 @@ func PrepareEnvironmentAndRunCommand(userConfig config.UserConfigModel, cmdName 
 		return 0, fmt.Errorf("Failed to create workspace root directory (path: %s), error: %s", wsConfig.WorkspaceRootPath, err)
 	}
 
-	if err := createGopathBinSymlink(origGOPATH, wsConfig); err != nil {
+	if err := gows.CreateGopathBinSymlink(origGOPATH, wsConfig.WorkspaceRootPath); err != nil {
 		return 0, fmt.Errorf("Failed to create GOPATH/bin symlink, error: %s", err)
 	}
 
@@ -108,7 +109,7 @@ func PrepareEnvironmentAndRunCommand(userConfig config.UserConfigModel, cmdName 
 			}
 
 			log.Debugf("=> Creating Symlink: (%s) -> (%s)", currWorkDir, fullPackageWorkspacePath)
-			if err := createOrUpdateSymlink(currWorkDir, fullPackageWorkspacePath); err != nil {
+			if err := gows.CreateOrUpdateSymlink(currWorkDir, fullPackageWorkspacePath); err != nil {
 				return 0, fmt.Errorf("Failed to create Project->Workspace symlink, error: %s", err)
 			}
 			log.Debugf(" [DONE] Symlink is in place")
@@ -200,65 +201,6 @@ func syncDirWithDir(syncContentOf, syncIntoDir string) error {
 		}
 		return fmt.Errorf("Failed to rsync between (%s) and (%s), error: %s", syncContentOf, syncIntoDir, err)
 	}
-	return nil
-}
-
-func createOrUpdateSymlink(symlinkTargetPath, symlinkLocationPath string) error {
-	fileInfo, isExists, err := pathutil.PathCheckAndInfos(symlinkLocationPath)
-	if err != nil {
-		return fmt.Errorf("Failed to check Symlink status (at: %s), error: %s", symlinkLocationPath, err)
-	}
-	isSymlinkAlreadyInPlace := false
-	if isExists && fileInfo.Mode()&os.ModeSymlink != 0 {
-		log.Debug(" Symlink already exists")
-		originPth, err := os.Readlink(symlinkLocationPath)
-		if err != nil {
-			return fmt.Errorf("Symlink found (at: %s), but failed to open: %s", symlinkLocationPath, err)
-		}
-
-		if originPth == symlinkTargetPath {
-			isSymlinkAlreadyInPlace = true
-		} else {
-			// remove
-			log.Warningf("Symlink already exists (at: %s), but target (%s) is not the current one (%s)", symlinkLocationPath, originPth, symlinkTargetPath)
-			log.Warning("Removing and re-creating the symlink ...")
-			if err := os.Remove(symlinkLocationPath); err != nil {
-				return fmt.Errorf("Failed to remove Symlink (at: %s), error: %s", symlinkLocationPath, err)
-			}
-		}
-	}
-
-	if !isSymlinkAlreadyInPlace {
-		log.Debug(" Creating symlink ...")
-		// create the parent directory
-		if err := os.MkdirAll(filepath.Dir(symlinkLocationPath), 0777); err != nil {
-			return fmt.Errorf("Failed to create base directory for symlink into: %s", symlinkLocationPath)
-		}
-		// create symlink
-		if err := os.Symlink(symlinkTargetPath, symlinkLocationPath); err != nil {
-			return fmt.Errorf("Failed to create symlink from project directory (%s) into gows Workspace directory (%s), error: %s", symlinkTargetPath, symlinkLocationPath, err)
-		}
-	}
-
-	return nil
-}
-
-func createGopathBinSymlink(origGOPATH string, wsConfig config.WorkspaceConfigModel) error {
-	fullWorkspaceBinPath := filepath.Join(wsConfig.WorkspaceRootPath, "bin")
-	originalGopathBinPath, err := pathutil.AbsPath(filepath.Join(origGOPATH, "bin"))
-	if err != nil {
-		return fmt.Errorf("Failed to get the path of 'bin' dir inside your GOPATH (%s), error: %s", origGOPATH, err)
-	}
-
-	log.Debugf("=> Creating Symlink: (%s) -> (%s)", originalGopathBinPath, fullWorkspaceBinPath)
-
-	// create symlink for GOPATH/bin, if not yet created
-	if err := createOrUpdateSymlink(originalGopathBinPath, fullWorkspaceBinPath); err != nil {
-		return fmt.Errorf("Failed to create GOPATH/bin symlink, error: %s", err)
-	}
-
-	log.Debugf(" [DONE] Symlink is in place")
-
 	return nil
 }
 
