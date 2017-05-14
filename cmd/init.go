e4fb47946c852c6f46d28b27a4d0d1a02f5eb0ac
Viktor Benei <viktor.benei@gmail.com>
2017-05-14 16:20:26 +0200

    golang pkg: moved ParsePackageNameFromURL from cmd/init to it (#5)
    * golang pkg: moved ParsePackageNameFromURL from cmd/init to it
    
    * just a function comment

--- cmd/init.go
+++ cmd/init.go
@@ -3,7 +3,6 @@ package cmd
 import (
 	"errors"
 	"fmt"
-	"net/url"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -13,6 +12,7 @@ import (
 	log "github.com/Sirupsen/logrus"
 	"github.com/bitrise-io/go-utils/colorstring"
 	"github.com/bitrise-tools/gows/config"
+	"github.com/bitrise-tools/gows/goutil"
 	"gopkg.in/viktorbenei/cobra.v0"
 )
 
@@ -69,41 +69,6 @@ func init() {
 		"Delete previous workspace (if any) and initialize a new one")
 }
 
-func parsePackageNameFromURL(remoteURL string) (string, error) {
-	origRemoteURL := remoteURL
-	if strings.HasPrefix(remoteURL, "git@") {
-		remoteURL = "ssh://" + remoteURL
-	}
-
-	u, err := url.Parse(remoteURL)
-	if err != nil {
-		return "", fmt.Errorf("Failed to parse remote URL (%s): %s", origRemoteURL, err)
-	}
-
-	packagePth := u.Path
-	packagePth = strings.TrimSuffix(packagePth, ".git")
-
-	// in SSH git urls like "ssh://git@github.com:bitrise-io/go-utils.git" Go parses "github.com:bitrise-io" as the host
-	// fix it by splitting it and replacing ":" with "/"
-	hostSplits := strings.Split(u.Host, ":")
-	host := hostSplits[0]
-	if len(hostSplits) > 1 {
-		if len(hostSplits) > 2 {
-			return "", fmt.Errorf("More than one ':' found in the Host part of the URL (%s)", origRemoteURL)
-		}
-		packagePth = "/" + hostSplits[1] + packagePth
-	}
-
-	if host == "" {
-		return "", fmt.Errorf("No Host found in URL (%s)", origRemoteURL)
-	}
-	if packagePth == "" || packagePth == "/" {
-		return "", fmt.Errorf("No Path found in URL (%s)", origRemoteURL)
-	}
-
-	return host + packagePth, nil
-}
-
 // AutoScanPackageName ...
 func AutoScanPackageName() (string, error) {
 	cmd := exec.Command("git", "remote", "get-url", "origin")
@@ -120,7 +85,7 @@ func AutoScanPackageName() (string, error) {
 	outStr := string(out)
 	gitRemoteStr := strings.TrimSpace(outStr)
 	log.Debugf("Found Git Remote: %s", gitRemoteStr)
-	packageName, err := parsePackageNameFromURL(gitRemoteStr)
+	packageName, err := goutil.ParsePackageNameFromURL(gitRemoteStr)
 	if err != nil {
 		return "", fmt.Errorf("Failed to parse package name from remote URL (%s), error: %s", outStr, err)
 	}
